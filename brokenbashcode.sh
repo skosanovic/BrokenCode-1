9e5dcd5f84766de73661b7a25236e1b5def0bf30
skosanovic <100165904+skosanovic@users.noreply.github.com>
2022-02-28 15:21:07 -0700

    Update brokenbashcode.sh

--- brokenbashcode.sh
+++ brokenbashcode.sh
@@ -10,4 +10,5 @@ echo -e "What is your favorite color?: "
 read color
 echo -e "What is your favorite activity?: "
 read hobby
+#Each variable in this line had an "s" after it, I went through and removed the s's so that the program would detect the variables and display the required info!
 echo "It's so cool that your name is $name and that you're $age years old. $origin sounds like a neat place to grow up! My favorite color is $color too! Maybe we could try $hobby together sometime!"
