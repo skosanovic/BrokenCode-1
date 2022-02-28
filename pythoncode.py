e8924a9ba262587b96c37e1239580ee66c62f93e
IAndrewJJ <100313205+IAndrewJJ@users.noreply.github.com>
2022-02-28 14:39:12 -0700

    Create pythoncode.py

---
+++ pythoncode.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+c1 = int(input('How old are you? '))
+
+if c2 > int(0) and c1  < int(18):
+    print('You are a child.')
+elif c1 > int(17):
+    print('You are an adult.')
+elif c1 == int(0)
+    print('You cannot be 0 years old.')
