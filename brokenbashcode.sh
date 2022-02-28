3cb1718d3aa0038efd40aefafc73f767e2f7d87e
IAndrewJJ <100313205+IAndrewJJ@users.noreply.github.com>
2022-02-28 14:56:11 -0700

    Create brokenbashcode.sh

---
+++ brokenbashcode.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+!echo -e "What is your name?:  "
+read name
+echo -e "How old are you?: "
+read age
+echo -e "Where are you from?: "
+read origin
+echo -e "What is your favorite color?: "
+read color
+echo -e "What is your favorite activity?: "
+read hobby
+echo "It's so cool that your name is $names and that you're $ages years old. $origins sounds like a neat place to grow up! My favorite color is $colors too! Maybe we could try $hobbys together sometime!"
